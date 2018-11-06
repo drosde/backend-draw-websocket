14b29f5b34928ea8c1c1d6f902cf4f1b5659ae46
drosde <enagu5@hotmail.com>
2018-11-06 00:44:04 -0300

    Added "new davinci" feature and new word

--- app/index.js
+++ app/index.js
@@ -11,16 +11,19 @@ var words = ('remera,iglesia,perro,gato,mariposa,cerveza,hacha,escuela'+
 var rooms = [{
     id: 'room1',
     clients: [],
+    playerTurnID: "",
     word: "",
     wordHint: ""
 },{
     id: 'room2',
     clients: [],
+    playerTurnID: "",
     word: "",
     wordHint: ""
 },{
     id: 'room3',
     clients: [],
+    playerTurnID: "",
     word: "",
     wordHint: ""
 }];
@@ -88,42 +91,61 @@ function onConnection(socket){
         console.log(`Usuario desconectado ${socket.id}, estaba en una sala? ${ b ? 'si, sala '+b : 'no'}`);        
     });
 
-    socket.on('drawed-data', (msg) => {
-        socket.broadcast.in(msg.room).emit('drawed-data', msg);
-    });
-
-    socket.on('chat-message', (message) => {
-        io.sockets.in(message.room).emit('chat-message', message);
-    });
-
-    socket.on('get-users-in-room', (room) => {
-        io.sockets.to(socket.id).emit('get-users-in-room', message);
-    });
+    // socket.on('get-users-in-room', (room) => {
+    //     io.sockets.to(socket.id).emit('get-users-in-room', message);
+    // });
 }
 
+// Once a user join a room
 function onSocketJoinRoom(socket, data){
-    let r = getRoomById(data.room);
+    let c_room = getRoomById(data.room);
     let user = {
         username: data.username,
-        drawing: r.clients.length == 0 ? true : false,
+        // drawing: c_room.clients.length == 0 ? true : false,
         points: 0,
         id: socket.id,
     }
 
-    if(r) r.clients.push(user)
-    console.log("Ingresando usuario a la sala: ", data.room + " "+ data.username);
+    if(c_room.clients.length == 0){        
+        // without timeout the clients doesn't receive this
+        setTimeout(() => changeDavinci(data.room, socket.id), 500);
+    }
+
+    if(c_room) c_room.clients.push(user)
+    console.log("Ingresando usuario a la sala: ", data.room + " "+ data.username);    
+
+    // RECEIVE DRAW EVENT FROM Davinci AND BROADCAST TO USERS IN ROOM: MSG.ROOM
+    socket.on('drawed-data', (msg) => {
+        socket.broadcast.in(msg.room).emit('drawed-data', msg);
+    });
+
+    // SAME AS ABOVE
+    socket.on('chat-message', (message) => {
+        // send the message to the rest
+        io.sockets.in(message.room).emit('chat-message', message);
+
+        let room = getRoomById(message.room);
+        console.log(`Word: ${room.word}`);
+
+        // victory
+        if(message.content == room.word){
+            console.log(`User ${message.author} WON THE GAME!`);
+
+            let davinci = getNewDavinci(room);
+            io.sockets.in(message.room).emit('game-davinci-update', davinci.id);
+
+            room.word = getNewWord();
+            room.wordHint = "_".repeat(room.word.length);
+
+            console.log('New davinci!:', davinci);
+        }
+    });
 
     // SEND USER CONNECTED EVENT TO ALL USERS IN THAT ROOM
     socket.broadcast.in(data.room).emit('user-connected-room', user);
 
-    // SEND USERS IN THAT ROOM TO USER CONNECTED
-    io.sockets.to(socket.id).emit('users-in-room', r.clients);
-
-    // SEND WORD DATA TO CONNECTED
-    io.sockets.to(socket.id).emit('game-word-update', {
-        wordLength: r.word.length,
-
-    });
+    // SEND INFO OF THAT ROOM TO USER CONNECTED
+    io.sockets.to(socket.id).emit('room-info', {clients: c_room.clients, playerTurnID: c_room.playerTurnID});
 }
 
 /**
@@ -171,4 +193,39 @@ function isCharAtSimilar(str, indx, sim){
 	if(indx > str.length -1) return;
 	var char = str.charAt(indx);
 	return char === sim ? true : false; 
+}
+
+function getNewDavinci(room){
+    if(room){
+        let next = 0;
+        let currDI = room.clients.findIndex(us => us.id == room.playerTurnID);
+        if(currDI != room.clients.length -1){
+            next = currDI + 1;
+        }        
+        let nextDavinci = room.clients[next];
+
+        room.playerTurnID = nextDavinci.id;
+        return nextDavinci;
+    }else{
+        return null;
+    }
+
+//     var us = rooms[rand];
+// if(us.id.charAt(0) == "b"){
+// 	while(us.id.charAt(0) == "b"){
+// 		var r = randomInt(rooms.length -1);
+// 		us = rooms[r];
+// 	}
+// 	console.log(us)
+// }
+}
+
+function changeDavinci(roomID, playerTurnID){
+    // io.sockets.in(roomID).emit('game-davinci-update', playerTurnID);
+    io.sockets.in(roomID).emit('game-davinci-update', playerTurnID);
+
+    let room = getRoomById(roomID);
+    if(room){
+        room.playerTurnID = playerTurnID;
+    }
 }
