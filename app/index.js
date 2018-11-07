a37f4daf339cea4a2372ed116a3a23460eb5559d
drosde <enagu5@hotmail.com>
2018-11-07 15:29:08 -0300

    real-time changes of the hint

--- app/index.js
+++ app/index.js
@@ -12,29 +12,49 @@ var rooms = [{
     id: 'room1',
     clients: [],
     playerTurnID: "",
-    word: "",
-    wordHint: ""
+    word: "",    
+    gameHelpers: {
+        wordHint: "",
+        lastWordUpdate: new Date(),
+        shuffledWord: [],
+        intvHintUpdt: null
+    },
 },{
     id: 'room2',
     clients: [],
     playerTurnID: "",
-    word: "",
-    wordHint: ""
+    word: "",    
+    gameHelpers: {
+        wordHint: "",
+        lastWordUpdate: new Date(),
+        shuffledWord: [],
+        intvHintUpdt: null
+    },
 },{
     id: 'room3',
     clients: [],
     playerTurnID: "",
-    word: "",
-    wordHint: ""
+    word: "",    
+    gameHelpers: {
+        wordHint: "",
+        lastWordUpdate: new Date(),
+        shuffledWord: [],
+        intvHintUpdt: null
+    },
 }];
 
 rooms.forEach(room => {
     room.word = getNewWord();
     // room.tempWord = room.word; 
     let r = randomInt(room.word.length - 1);
-    room.wordHint = "_".repeat(room.word.length);
-    room.wordHint = setCharAt(room.wordHint, r, room.word.charAt(r));
-    // room.wordHint = isCharAtSimilar(room.wordHint,0, "_") ? setCharAt(room.wordHint, r, room.word.charAt(r)) : room.wordHint;
+    room.gameHelpers.wordHint = "_".repeat(room.word.length);
+    // room.gameHelpers.wordHint = setCharAt(room.gameHelpers.wordHint, r, room.word.charAt(r));
+
+    let w_array = [];
+        room.word.split("").map(function(ltt, i){ w_array[i] = {pos: i, ltt: ltt}});
+
+    room.gameHelpers.shuffledWord = shuffle(w_array);
+    // room.gameHelpers.wordHint = isCharAtSimilar(room.wordHint,0, "_") ? setCharAt(room.wordHint, r, room.word.charAt(r)) : room.wordHint;
 })
 
 /**
@@ -106,9 +126,13 @@ function onSocketJoinRoom(socket, data){
         id: socket.id,
     }
 
+    // if room is empty
     if(c_room.clients.length == 0){        
         // without timeout the clients doesn't receive this
         setTimeout(() => changeDavinci(data.room, socket.id), 500);
+
+        // Start sending hints
+        c_room.gameHelpers.intvHintUpdt = setInterval(() => initHintInterval(c_room), 18000 / 2.2);
     }
 
     if(c_room) c_room.clients.push(user)
@@ -128,14 +152,18 @@ function onSocketJoinRoom(socket, data){
         console.log(`Word: ${room.word}`);
 
         // victory
-        if(message.content == room.word){
+        if(message.content == room.word && socket.id != room.playerTurnID){
             console.log(`User ${message.author} WON THE GAME!`);
 
             let davinci = getNewDavinci(room);
             io.sockets.in(message.room).emit('game-davinci-update', davinci.id);
 
             room.word = getNewWord();
-            room.wordHint = "_".repeat(room.word.length);
+            room.gameHelpers.wordHint = "_".repeat(room.word.length);
+            room.lastWordUpdate = new Date();
+
+            let user = room.clients.find(user => user.id == socket.id);
+            io.sockets.in(message.room).emit('game-points-update', {user: socket.id, points: user.points + 50});
 
             console.log('New davinci!:', davinci);
         }
@@ -145,7 +173,7 @@ function onSocketJoinRoom(socket, data){
     socket.broadcast.in(data.room).emit('user-connected-room', user);
 
     // SEND INFO OF THAT ROOM TO USER CONNECTED
-    io.sockets.to(socket.id).emit('room-info', {clients: c_room.clients, playerTurnID: c_room.playerTurnID});
+    io.sockets.to(socket.id).emit('room-info', {clients: c_room.clients, playerTurnID: c_room.playerTurnID, wordLength: c_room.word.length});
 }
 
 /**
@@ -189,10 +217,12 @@ function setCharAt(str, index, chr){
     return str.substr(0, index) + chr + str.substr(index + 1);
 }
 
-function isCharAtSimilar(str, indx, sim){
-	if(indx > str.length -1) return;
-	var char = str.charAt(indx);
-	return char === sim ? true : false; 
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
 function getNewDavinci(room){
@@ -209,23 +239,29 @@ function getNewDavinci(room){
     }else{
         return null;
     }
-
-//     var us = rooms[rand];
-// if(us.id.charAt(0) == "b"){
-// 	while(us.id.charAt(0) == "b"){
-// 		var r = randomInt(rooms.length -1);
-// 		us = rooms[r];
-// 	}
-// 	console.log(us)
-// }
 }
 
 function changeDavinci(roomID, playerTurnID){
-    // io.sockets.in(roomID).emit('game-davinci-update', playerTurnID);
     io.sockets.in(roomID).emit('game-davinci-update', playerTurnID);
 
     let room = getRoomById(roomID);
     if(room){
         room.playerTurnID = playerTurnID;
+    }
+}
+
+//Calculate match time with Math.abs(date1 - date2) / (1000 * 60)
+function initHintInterval(room){
+    if(room){
+        let poped = room.gameHelpers.shuffledWord.pop();
+        if(poped){
+            room.gameHelpers.wordHint = setCharAt(room.gameHelpers.wordHint, poped.pos, poped.ltt);
+            console.log(`New letter ADDED: POSITION ${poped.pos}, LETTER ${poped.ltt}.`);
+            console.log(`Updated room hint: ${room.gameHelpers.wordHint} Shufled Word: ${JSON.stringify(room.gameHelpers.shuffledWord)}`);
+
+            io.sockets.in(room.id).emit('game-word-update', {type: 'hint-update', hint: room.gameHelpers.wordHint});
+        }else{
+            // TIME IS OVER. GET NEW WORD AND NEW DAVINCI
+        }
     }
 }
